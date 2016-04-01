e09549395a205f787dec97734b20a5b8e8a4b86e
samchen <samchen61661@gmail.com>
2016-04-01 19:01:22 -0400

    fix bug

--- lib/recursion.py
+++ lib/recursion.py
@@ -1,21 +1,23 @@
 import math
 
-
 def recursion(memo, row_coll, col_coll, row_index, col_index, direc):
 	#max_row = len(memo)
 	#max_col = len(memo[0])
 	if row_index == 0 and col_index == 0:
 		return True
-	if (row_index - 1) < 0 or (col_index - 1) < 0:
+	if (row_index) < 0 or (col_index) < 0:
 		return False
 	#record the medium one	
 	distance = []
 	# 1: left up 
 	# 2: right up
 	# 3: left down
-	distance.append(str(memo[row_index - 1][col_index - 1]) + "+" + "1")
-	distance.append(str(memo[row_index - 1][col_index]) + "+" + "2")
-	distance.append(str(memo[row_index][col_index - 1]) + "+" + "3")
+	if (row_index - 1 >= 0) and (col_index - 1 >= 0):
+		distance.append(str(memo[row_index - 1][col_index - 1]) + "+" + "1")
+	if (row_index - 1 >= 0):
+		distance.append(str(memo[row_index - 1][col_index]) + "+" + "2")
+	if col_index - 1 >= 0:
+		distance.append(str(memo[row_index][col_index - 1]) + "+" + "3")
 	distance.sort()
 	#mini = distance[0].split(",")[1]
 	#medi = distance[1].split(",")[1]
@@ -26,9 +28,10 @@ def recursion(memo, row_coll, col_coll, row_index, col_index, direc):
 	print distance
 	''' 
 	for i in range(3):
+		#print distance
 		direction = distance[i].split("+")[1]
 		#print "direction: " + str(direction)
-		if direction == "1":
+		if (direction == "1"):
 			if recursion(memo, row_coll, col_coll, row_index - 1, col_index - 1, direc) == True:
 				row_coll.append(row_index)
 				col_coll.append(col_index)
@@ -41,7 +44,7 @@ def recursion(memo, row_coll, col_coll, row_index, col_index, direc):
 				direc.append("up")
 				return True
 		else:
-			if recursion(memo, row_coll, col_coll, row_index, col_index - 1) == True:
+			if recursion(memo, row_coll, col_coll, row_index, col_index - 1, direc) == True:
 				row_coll.append(row_index)
 				col_coll.append(col_index)
 				direc.append("left")
