f9e9d474b4226fd09d7afec211e7d76c9be351b9
tq5124 <tq5124.web@gmail.com>
2016-04-03 16:12:01 -0400

    print log

--- main.py
+++ main.py
@@ -7,16 +7,20 @@ from lib.question_type import question_type
 from spacy.en import English
 
 nlp = None
+f_out = None
 
 def main():
-    if len(sys.argv) != 3:
-        print 'usage: python main.py input.txt question.txt'
+    if len(sys.argv) != 4:
+        print 'usage: python main.py input.txt question.txt output'
         sys.exit(1)
 
     print 'Reading data for NLP task...'
     global nlp
     nlp = English()
     print 'Done\n'
+
+    global f_out
+    f_out = codecs.open(sys.argv[3], 'w', 'utf-8')
 
     docs = read_sentences_from_file(sys.argv[1])
     docs_q = read_sentences_from_file(sys.argv[2], False, True)
@@ -66,7 +70,7 @@ def read_sentences_from_file(file_name, wiki=True, question=False):
     return docs
 
 def get_string_of_sent(sent):
-    return ' '.join([token.orth_ for token in sent])
+    return unicode(' '.join([token.orth_ for token in sent]))
 #
 # def get_string_of_text(sent_array):
 #     text = []
@@ -82,7 +86,7 @@ def asking(docs, num_easy=5, num_medium=5, num_hard=5):
     #     "Weixiang was born in China in 1993 .",
     #     "Guanxi is taking photos ."
     # ]
-    print "Easy\n"
+    f_out.write("Easy\n")
     count = 0
     for doc in docs:
         if count >= num_easy:
@@ -90,23 +94,22 @@ def asking(docs, num_easy=5, num_medium=5, num_hard=5):
         sen = get_string_of_sent(doc)
         result = asking_easy.easy_question_generator(sen)
         if result:
-            print result
+            f_out.write(result)
             # print sen + '\n'
-            print '\n'
             count += 1
 
-    print "Medium\n"
+    f_out.write("Medium\n")
     count = 0
     for doc in docs:
         if count >= num_medium:
             break
         result = asking_medium.medium_question_generator(doc, get_string_of_sent(doc))
         if result:
-            print result
+            f_out.write(result)
             # print doc, '\n'
             count += 1
 
-    print "Hard\n"
+    f_out.write("Hard\n")
     count = 0
     for doc in docs:
         if count >= num_hard:
@@ -114,7 +117,7 @@ def asking(docs, num_easy=5, num_medium=5, num_hard=5):
         sen = get_string_of_sent(doc)
         result = asking_hard.hard_question_generator(sen)
         if result:
-            print result
+            f_out.write(result)
             # print sen + '\n'
             count += 1
 
@@ -127,7 +130,8 @@ def get_root_of_doc(doc):
 def answering(docs, docs_q):
     # text_2d_array = get_string_of_text(sentences)
     for question_doc in docs_q:
-        print 'Answering:', question_doc
+        print 'Answering:' + get_string_of_sent(question_doc) + '\n'
+        f_out.write('Answering:' + get_string_of_sent(question_doc) + '\n\n')
         type_of_question = question_type(get_string_of_sent(question_doc), get_root_of_doc(question_doc))
         # print type_of_question
         possible_sentences_index, possible_sentences_prob = answer.find_possible_sentences(docs, question_doc)
@@ -140,22 +144,23 @@ def answering(docs, docs_q):
         # print spacy_helper.doc_get_all_sents(doc)[possible_sentences_index[0]]
 
         possible_sentences = []
-        print 'Possible sentences:'
+        f_out.write('Possible sentences:\n')
         for index in possible_sentences_index:
-            print get_string_of_sent(docs[index])
+            f_out.write(get_string_of_sent(docs[index]) + '\n')
             possible_sentences.append(docs[index])
-        print
+        f_out.write('\n')
 
         # question_answer = answer.answer_yesno(question_doc, possible_sentences)
         # print question_answer
         # break
-        print 'Answer:'
+        f_out.write('Answer:' + '\n')
         question_answer = None
         if type_of_question == 'WH':
             question_answer = answer.answer_what(question_doc, possible_sentences, possible_sentences_prob)
+            question_answer = get_string_of_sent(question_answer)
         elif type_of_question == 'YES/NO':
             question_answer = answer.answer_yesno(question_doc, possible_sentences)
 
-        print question_answer, '\n'
+        f_out.write(question_answer + '\n\n')
 
 main()
