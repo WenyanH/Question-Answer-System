0adc43d5bf03605ad809fc447cd5b777b169625f
samchen61661 <samchen61661@gmail.com>
2016-04-05 15:55:53 -0400

    add root

--- lib/asking_easy.py
+++ lib/asking_easy.py
@@ -1,5 +1,5 @@
 import string
-def easy_question_generator(line):
+def easy_question_generator(line, position):
 	#collection
 	col = ["is", "are", "am", "was", "were", "can", "could", "must", "should", "may", "might", "will", "would", "shall"]
 	col_up = ["Is", "Are", "Am", "Was", "Were", "Can", "Could", "Must", "Should", "May", "Might", "Will", "Would", "Shall"]
@@ -7,9 +7,50 @@ def easy_question_generator(line):
 	demon = ["it", "its", "this", "that", "those", "these"]
 	demon_up = ["It", "Its", "This", "That", "Those", "These"]
 
+	if position == 0:
+		return "NO SENTENCE"
 
 	col_out = []
 	line = line.strip()
+	arr = line.split(" ")
+	mark = -1
+	#determine whether number or alphabet
+	for i in range(position):
+		sign = False
+		for m in range(len(arr[i])):
+			if arr[i].strip()[m] in string.ascii_letters or arr[i].strip()[m] in string.digits:
+				sign = True
+				break
+		#record the location of none-alnum
+		if sign == False:
+			mark = i
+	question_word = -1
+	for i in range(mark + 1, position + 1):
+		if i > 0 and arr[i] in col and arr[i - 1] not in demon and arr[i - 1] not in demon_up:
+			question_word = i;
+			break;
+	if question_word == -1:
+		return "NO SENTENCES"
+	for i in range(mark + 1):
+		col_out.append(arr[i])
+	#print mark
+	#print type(mark)
+	if (mark == -1):
+		col_out.append(arr[question_word][0].upper() + arr[question_word][1: len(arr[question_word])])
+	else:
+		col_out.append(arr[question_word])
+
+	for i in range(mark + 1, len(arr) - 1):
+		if i == question_word:
+			continue
+		else:
+			col_out.append(arr[i])
+	col_out.append("?")
+	#for m in col_out:
+	#	print m,
+	return ' '.join(col_out)
+
+	'''
 	for i in range(len(col)):
 		#if the sentence has the key words
 		if col[i] in line:
@@ -86,8 +127,10 @@ def easy_question_generator(line):
 			break;		
 		else:
 			continue;
+	'''
 		
 if __name__ == "__main__":
-	print easy_question_generator("Guanxi is taking photos .")
-	print easy_question_generator("Guanxi is not taking photos .")
-	print easy_question_generator("Haha ni hao .")
+	#print easy_question_generator("Guanxi takes photos .", 1)
+	print easy_question_generator("Guanxi can not taking photos when he is at home .", 2)
+	print easy_question_generator("When he is at home , Guanxi can not taking photos .", 9)
+	print easy_question_generator("Haha ni hao .", 3)
