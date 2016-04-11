8875d09761485953d96453b2f83c4da934e6960e
Weixiang Ding <dingwx199@gmail.com>
2016-04-11 16:07:25 -0400

    question_evalution

--- lib/question_evaluation.py
+++ lib/question_evaluation.py
@@ -3,9 +3,12 @@ import heapq
 
 def question_evalution(question_doc_list, wiki_doc_list, n):
 	heap = []
-	total_number_questions = len(question_doc_list)
+	final_questions = []
+	total_number_questions = len(wiki_doc_list)
 	for sent in question_doc_list:
 		number_of_words = len(sent)
+		if number_of_words <= 6:
+			continue
 		weight = 0
 		frequency_in_sentence = {}
 		exist_token = {}
@@ -16,27 +19,27 @@ def question_evalution(question_doc_list, wiki_doc_list, n):
 				frequency_in_sentence[token.lemma_] = 1
 		for token in sent:
 			count = 0
-			for sent2 in docs:
+			for sent2 in wiki_doc_list:
 				for token2 in sent2:
 					if token2.lemma_ == token.lemma_:
 						count = count + 1
 						break
-			weight_temp = frequency_in_sentence[token.lemma_] * math.log(total_number_questions / count)
-			for token_question in question:
-				if token_question.lemma_ == token.lemma_:
-					if token.lemma_ in exist_token:
-						continue
-					else:
-						weight += weight_temp
-						exist_token[token.lemma_] = 1
+			if count == 0:
+				weight_temp = 0
+			else:
+				weight_temp = frequency_in_sentence[token.lemma_] * math.log(total_number_questions / count)
+			weight += weight_temp
 		#print weight
-		weight = weight / number_of_words
+		weight = weight / (number_of_words**2)
 		s = ''
 		for word in sent:
-			s += word + ' '
+			s += word.orth_ + ' '
 		s = s.rstrip()
-		s = s + '?'
 		heapq.heappush(heap, (weight * -1, s))
+
+	for i in range(n):
+		final_questions.append(heapq.heappop(heap)[1])
+	return final_questions
 
 if __name__ == "__main__":
 	pass
